bb2fab96a3efef9ae8c05e287be9205e7873ac24
Seth VanderZanden <sethvanderzanden28@gmail.com>
2022-03-01 19:29:57 -0500

    Project Euler updated

---
+++ ProjectEuler/Challenge15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Challenge15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int grid = 20;
+            long answer = 1;
+
+            for (int i = 0; i < grid; i++)
+            {
+                answer *= (2 * grid) - i;
+                answer /= i + 1;
+            }
+
+            Console.WriteLine(answer);
+
+        }
+    }
+}
---
+++ ProjectEuler/Challenge16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace Challenge16
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+                What is the sum of the digits of the number 2^1000? */
+
+            // get the answer
+            BigInteger sum = (BigInteger)Math.Pow(2, 1000);
+
+            // convert to string
+            string sumString = sum.ToString();
+
+            // answer we will need
+            BigInteger answer = 0;
+
+            // get each char and convert to bigint
+            foreach(char c in sumString)
+            {
+                answer += (BigInteger)Char.GetNumericValue(c);
+            }
+
+            // final answer
+            Console.WriteLine(answer);
+        }
+
+    }
+}
