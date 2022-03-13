8c7938241bf8ef0e07c4a2de9b99f2574c3befeb
Seth VanderZanden <sethvanderzanden28@gmail.com>
2022-03-13 15:56:41 -0400

    added

---
+++ ProjectEuler/Challenge20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Program {
+    class Challenge19 {
+        public static void Main(string[] args) {
+            BigInteger bi = 1;
+
+            for(int i = 1; i < 100; ++i) {
+                bi *= i;
+            }
+
+            //System.Console.WriteLine(bi);
+            string s = bi.ToString();
+            long sum = 0;
+            foreach(char c in s) {
+                sum += (long)Char.GetNumericValue(c);
+            }
+
+            System.Console.WriteLine(sum);
+        }
+    } 
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace Program {
+    class Challenge {
+        public static void Main(string[] args) {
+            
+        }
+    } 
+}
